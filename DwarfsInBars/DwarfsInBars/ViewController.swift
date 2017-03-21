2b9f6fbdccec43137d4f6842ec13e4b214c969c5
Ludovic Ollagnier <ludovic@tectec.fr>
2017-03-21 16:53:49 +0100

    First demo with outlets and actions

--- DwarfsInBars/DwarfsInBars/ViewController.swift
+++ DwarfsInBars/DwarfsInBars/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var helloTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func test(_ sender: Any) {
+        
+    }
+    
+    @IBAction func helloWorld(_ sender: UIButton) {
+        
+        helloWorldLabel.text = helloTextField.text
+        print("hello world")
+    }
 
 }
 
